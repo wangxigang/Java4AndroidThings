b5f8151ed4a740e3d5c206d4193b441e999bdd06
wangxigang <wangxigang.dalian@gmail.com>
2017-07-07 14:26:20 +0800

    Begining
    基础开发环境
    -OpenCV导入完毕

---
+++ app/src/main/java/com/dxwsoft/arbim/java4androidthings/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dxwsoft.arbim.java4androidthings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
